d3d18ae041727c80de7be4f8dd4d0835d48b2162
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-08-17 17:00:20 +0800

    Navigation controller Loops

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,8 +15,17 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        titleLabel.text = ""
+        var characterIndex = 0.0
+       let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            print("-")
+            print(0.1 * characterIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            characterIndex += 1
+        }
     }
-    
-
 }
