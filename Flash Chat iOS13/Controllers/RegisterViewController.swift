7a45451f001cf6b96dbc942a2807d94054c90b76
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-08-18 21:44:58 +0800

    Populate table view ok next listening for data added

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        guard let email = emailTextfield.text, let password = passwordTextfield.text else { return }
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let err = error {
+                print(err.localizedDescription)
+            } else {
+                // navigate to chatViewController
+                self.performSegue(withIdentifier: K.registerSegue, sender: self)
+            }
+        }
     }
-    
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         titleLabel.text = ""
         var characterIndex = 0.0
-       let titleText = "⚡️FlashChat"
+        let titleText = K.appName
         for letter in titleText {
-            print("-")
-            print(0.1 * characterIndex)
-            print(letter)
+            //print("-")
+            // print(0.1 * characterIndex)
+            //print(letter)
             Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { timer in
                 self.titleLabel.text?.append(letter)
             }
