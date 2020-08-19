afecabde439f6f6fb572681976e64525e45b5509
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-08-19 23:33:58 +0800

    UI ok need to deep dive

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -29,40 +29,52 @@ class ChatViewController: UIViewController {
     }
     
     private func loadMessages() {
-        messages = []
         
-        db.collection(K.FStore.collectionName).getDocuments { (querrySnapshot, error) in
-            if let err = error {
-                print("There was an issue retrieving data from Firestore. \(err)")
-            } else {
-                print("Successfully")
-                if let snapshotDocuments = querrySnapshot?.documents {
-                    for doc in snapshotDocuments {
-                        let data = doc.data()
-                        if  let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
-                            let newMessage = Message(sender: messageSender, body: messageBody)
-                            self.messages.append(newMessage)
-                            
-                            DispatchQueue.main.async {
-                                self.tableView.reloadData()
+        db.collection(K.FStore.collectionName).order(by: K.FStore.dateField)
+            .addSnapshotListener { (querrySnapshot, error) in
+                self.messages = []
+                if let err = error {
+                    print("There was an issue retrieving data from Firestore. \(err)")
+                } else {
+                    print("Successfully")
+                    if let snapshotDocuments = querrySnapshot?.documents {
+                        for doc in snapshotDocuments {
+                            let data = doc.data()
+                            if  let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                                let newMessage = Message(sender: messageSender, body: messageBody)
+                                self.messages.append(newMessage)
                                 
+                                
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
+                                    let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                    self.tableView.scrollToRow(at: indexPath , at: .top, animated: true)
+                                }
                             }
                         }
                     }
                 }
-            }
         }
     }
     
     
     @IBAction func sendPressed(_ sender: UIButton) {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { error in
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
+                
+            ]) { error in
                 if let err = error {
                     print("There was an issue saving data to firestore, \(err)")
                 } else {
                     print("Successfully saved data")
-                    self.messageTextfield.text = ""
+                    
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+
+                    }
                 }
             }
         }
@@ -86,8 +98,26 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label.text = messages[indexPath.row].body
+        cell.label.text = message.body
+        
+        //This is a message from the current user.
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }
+        //This is a message from another sender.
+        else {
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        
         return cell
     }
 }
